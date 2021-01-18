8fd986fed4b211c08b78606c16347ab1c2bdb45a
Sergio Lillo <Sergiolillo2315@gmail.com>
2021-01-18 17:39:46 -0300

    pruebas resolver con mvp

--- app/src/main/java/com/example/proyectofinaljava/second_activity/ModelSecondFrag.java
+++ app/src/main/java/com/example/proyectofinaljava/second_activity/ModelSecondFrag.java
@@ -12,7 +12,7 @@ public class ModelSecondFrag {
     private int pointBtn4;
     private int pointRdBtn;
     private int result;
-    private String colorBrutton = "#E80B0B";
+    private String colorBrutton;
     private String message;
     private String messageAlert;
     private boolean activeAlert;
@@ -35,48 +35,66 @@ public class ModelSecondFrag {
         return activeAlert;
     }
 
-    public void checkOpcion(boolean btn1, boolean btn2, boolean btn3, boolean btn4
-            , boolean rbBtn1, boolean rbBtn2){
+    public void checkOpcion(boolean btn1,boolean btn2,boolean btn3
+            ,boolean btn4,boolean rdBtn1,boolean rdBtn2,boolean rdBtn3) {
 
-        if (btn1==true) {
+        if (btn1 == true) {
             pointBtn1 = 10;
         } else {
             pointBtn1 = 0;
         }
-        if (btn2==true) {
-            pointBtn2 = 40;
+        if (btn2 == true) {
+            pointBtn2 = 15;
         } else {
             pointBtn2 = 0;
         }
-        if (btn3==true) {
+        if (btn3 == true) {
             pointBtn3 = 30;
         } else {
             pointBtn3 = 0;
         }
-        if (btn4==true) {
-            pointBtn4 = 20;
+        if (btn4 == true) {
+            pointBtn4 = 100;
         } else {
             pointBtn4 = 0;
         }
-        if (rbBtn1==true) {
+        if (rdBtn1 == true) {
             pointRdBtn = 3;
-        } else if (rbBtn2==true){
+        }
+        if (rdBtn2 == true) {
             pointRdBtn = 2;
-        }else {
+        }
+        if(rdBtn3 == true) {
             pointRdBtn = 1;
         }
-        result = (pointBtn1+pointBtn2+pointBtn3+pointBtn4)*pointRdBtn;
-    }
-    public void activateAlert(){
+        result = (pointBtn1 + pointBtn2 + pointBtn3 + pointBtn4) * pointRdBtn;
         if (result<130){
             message = "Envie una alerta por bajo puntaje ";
-            messageAlert = "Esto es un mensaje de alerta!";
-            activeAlert = true;
+            colorBrutton = "#E80B0B";
+            /*Toast.makeText(getContext(),"Envie una alerta por bajo puntaje ",Toast.LENGTH_SHORT).show();
+            binding.btnAlert.setBackgroundColor(Color.parseColor("#E80B0B"));
+            activeAlert = true;*/
         }else {
-            colorBrutton = "#676666";
             message = "Seleccione guardar";
-            messageAlert = "No puede enviar una alerta";
-            activeAlert = false;
+            colorBrutton = "#676666";
+            /*Toast.makeText(getContext(),"Seleccione guardar",Toast.LENGTH_SHORT).show();
+            binding.btnAlert.setBackgroundColor(Color.parseColor("#676666"));
+            activeAlert = false;*/
         }
     }
+   /* public void activateAlert(int result){
+        if (result<130){
+            message = "Envie una alerta por bajo puntaje ";
+            colorBrutton = "#E80B0B";
+            *//*Toast.makeText(getContext(),"Envie una alerta por bajo puntaje ",Toast.LENGTH_SHORT).show();
+            binding.btnAlert.setBackgroundColor(Color.parseColor("#E80B0B"));
+            activeAlert = true;*//*
+        }else {
+            message = "Seleccione guardar";
+            colorBrutton = "#676666";
+            *//*Toast.makeText(getContext(),"Seleccione guardar",Toast.LENGTH_SHORT).show();
+            binding.btnAlert.setBackgroundColor(Color.parseColor("#676666"));
+            activeAlert = false;*//*
+        }
+    }*/
 }
